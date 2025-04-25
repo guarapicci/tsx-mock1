741d2d7c735e25cb081cffca9b8bf5b21f978da0
Guarapicci <guarapicci@none>
2025-04-25 15:32:05 -0300

    page reset, first styled component

---
+++ app/lib/components.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const TopBar = styled.div`
+	width: 100%;
+	height: 80px;
+	background-color: cyan;
+	color: black;
+	padding: 12px;
+`
